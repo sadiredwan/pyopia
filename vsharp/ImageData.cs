3877cbfe0528345b0202e4b3457078cdf7ba4eec
sadiredwan <sadi.redwan@gmail.com>
2020-04-07 19:52:31 +0600

    .

---
+++ vsharp/ImageData.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vsharp
+{
+    public class ImageData
+    {
+        [LoadColumn(0)]
+        public string ImagePath;
+
+        [LoadColumn(1)]
+        public string Label;
+    }
+}
