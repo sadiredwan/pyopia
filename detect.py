f3cac5d688521de59b1ef3eec99e7bed7864b240
sadiredwan <sadi.redwan@gmail.com>
2020-04-18 16:06:31 +0600

    .

--- detect.py
+++ detect.py
@@ -1,13 +1,33 @@
 import cv2
 import numpy as np
 import dlib
+from math import hypot
 
 cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 
 def midpoint(p1 ,p2):
-    return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+	return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+	left_point = (facial_landmarks.part(eye_points[0]).x, facial_landmarks.part(eye_points[0]).y)
+	right_point = (facial_landmarks.part(eye_points[3]).x, facial_landmarks.part(eye_points[3]).y)
+	center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+	center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+	hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 1)
+	ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 1)
+
+	hor_line_lenght = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+	ver_line_lenght = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+	ratio = hor_line_lenght / ver_line_lenght
+	return ratio
+
 
 if __name__ == "__main__":
 	
@@ -21,13 +41,12 @@ if __name__ == "__main__":
 			# cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 1)
 
 			landmarks = predictor(gray, face)
-			left_mark = (landmarks.part(36).x, landmarks.part(36).y)
-			right_mark = (landmarks.part(39).x, landmarks.part(39).y)
-			top_mark = midpoint(landmarks.part(37), landmarks.part(38))
-			bottom_mark = midpoint(landmarks.part(41), landmarks.part(40))
+			left_eye_ratio = get_blinking_ratio([36, 37, 38, 39, 40, 41], landmarks)
+			right_eye_ratio = get_blinking_ratio([42, 43, 44, 45, 46, 47], landmarks)
+			blinking_ratio = (left_eye_ratio + right_eye_ratio) / 2
 
-			horizontal = cv2.line(frame, left_mark, right_mark, (0, 255, 0), 1)
-			vertical = cv2.line(frame, top_mark, bottom_mark, (0, 255, 0), 1)
+			if blinking_ratio > 4:
+				cv2.putText(frame, "BLINKING", (50, 150), font, 4, (255, 0, 0))
 
 
 		cv2.imshow("Frame", frame)
